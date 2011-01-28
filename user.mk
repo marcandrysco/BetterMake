674576e8b5569af636d660373f3bf991ef2ecc3c
Marc Andrysco <marc.andrysco@gmail.com>
2011-01-28 12:49:05 -0500

    updated to use bmake as the build system

--- user.mk
+++ user.mk
@@ -1,33 +1,44 @@
-PREFIX	= $(HOME)
-SHARE	= $(PREFIX)/share
-BIN	= $(PREFIX)/bin
+PREFIX = $(bmake_PATH_PREFIX)
+BIN = $(bmake_PATH_BIN)
+SHARE = $(bmake_PATH_SHARE)
+
 APPNAME = bmake
 VERSION = 0.1.1
 DISTNAME= $(APPNAME)-$(VERSION)
 
-all:
+
+all: user-all
+
+user-all:
 	@echo Nothing needs to be built
 
-install:
+install: user-install
+
+user-install:
 	for file in share/mktests/* ; do install --mode=0755 -D "$$file" $(SHARE)/bmake/mktests/"$${file##*/}" ; done
 	install --mode=0644 -D share/Makefile.in $(SHARE)/bmake/Makefile.in
 	install --mode=0755 -D share/configure $(SHARE)/bmake/configure
 	install --mode=0755 -D bmake $(BIN)/bmake
 	sed -i '/^sharedir=/ csharedir='\'"$(SHARE)/bmake"\' $(BIN)/bmake
 
-dist:
+dist: user-dist
+
+user-dist:
 	if [ -e $(DISTNAME) ] ; then rm -rf $(DISTNAME) ; fi
 	mkdir $(DISTNAME)
-	mkdir $(DISTNAME)/mktests
 	mkdir $(DISTNAME)/share
+	mkdir $(DISTNAME)/share/mktests
 	cp Makefile bmake $(DISTNAME)
-	cp mktests/[0-9][0-9]* $(DISTNAME)/mktests
+	cp share/mktests/[0-9][0-9]* $(DISTNAME)/share/mktests
 	cp share/Makefile.in share/configure $(DISTNAME)/share
 	tar -zcf $(DISTNAME).tar.gz $(DISTNAME)/
 	tar -jcf $(DISTNAME).tar.bz2 $(DISTNAME)/
 	rm -rf $(DISTNAME)
 
-clean:
+clean: user-clean
+
+user-clean:
 	rm -f $(DISTNAME).tar.gz $(DISTNAME).tar.bz2
 
-.PHONY: all install dist clean
+.PHONY: user-all user-install user-dist user-clean
+
