a388d350c87eb176c5ebf4fb2cc350e8a0f917f6
Marc Andrysco <marc.andrysco@gmail.com>
2011-10-10 03:15:35 -0400

    update build files, add config files to clean

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 #define BMAKE__PATH_PREFIX	"/home/marc"
 #define BMAKE__PATH_BIN		"/home/marc/bin"
 #define BMAKE__PATH_LIB		"/home/marc/lib"
+#define BMAKE__PATH_INCLUDE	"/home/marc/include"
 #define BMAKE__PATH_SHARE	"/home/marc/share"
 #define BMAKE__PATH_CONF	"/home/marc/etc"
 
